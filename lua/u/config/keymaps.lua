0e5bbe7c8d46c054126e925ab3996423a20b25f2
Marc Mazer <cramrezam@gmail.com>
2021-11-21 18:15:16 -0500

    Add misc mappings for indent guide and bufonly

--- lua/u/config/keymaps.lua
+++ lua/u/config/keymaps.lua
@@ -80,7 +80,10 @@ vmap('<space>z', 'za')
 -- refocus fold under cursor - from Steve Losh
 nmap(',z', 'zMzvzz')
 
-
 -- source vimscript visual selection or line - from Steve Losh
 vmap('X', "y:execute @@<cr>:echo 'Sourced selection.'<cr>")
 nmap('X', "^vg_y:execute @@<cr>:echo 'Sourced line.'<cr>")
+
+
+map("", '<C-g>', ':call indent_guide#toggle()<cr>')
+nmap('<leader>b', ':Bonly<cr>')
