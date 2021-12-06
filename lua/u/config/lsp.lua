e716a905863e7edd74dc3051834bd3d4bfefd878
Marc Mazer <cramrezam@gmail.com>
2021-12-06 10:33:53 -0500

    Fix vim.diagnostic deprecations
    See https://github.com/neovim/neovim/issues/14090#issuecomment-921312955

--- lua/u/config/lsp.lua
+++ lua/u/config/lsp.lua
@@ -1,24 +1,26 @@
 local nvim_lsp = require 'lspconfig'
 
 function PrintLspDiagnostics(opts, bufnr, line_nr, client_id)
-  local warnings = vim.lsp.diagnostic.get_count(0, "Warning")
-  local errors = vim.lsp.diagnostic.get_count(0, "Error")
+  local WARN = vim.diagnostic.severity.WARN
+  local ERROR = vim.diagnostic.severity.ERROR
+  local warnings = vim.diagnostic.get(0, {severity=WARN})
+  local errors = vim.diagnostic.get(0, {severity=ERROR})
   local diagnostic_message = ""
   diagnostic_message = diagnostic_message .. string.format("Warning(s): %d Error(s): %d", warnings, errors)
   vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
 end
 
--- customize diagnostic display
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+-- general lspc config
+vim.diagnostic.config({
   virtual_text = false,
-  signs = true,
   underline = true,
-  update_in_insert = false,
+  signs = true,
+  update_in_insert = false
 })
 
 -- This setting is global and should be set only once
 vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float()]]
 
 local on_attach = function(client, bufnr)
 
@@ -31,13 +33,13 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   local opts = { noremap=true, silent=true }
 
-  buf_set_keymap('n', 'dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', 'dl', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   buf_set_keymap('n', 'dg', '<cmd>lua PrintLspDiagnostics()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 end
 
 --- Python Language Server
