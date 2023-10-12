34d4c5eaa7b8e2bc56b6f593e75120587d555d9f
Marc Mazer <cramrezam@gmail.com>
2023-10-12 10:26:21 -0400

    Add command to show buffer file path

--- lua/u/config/commands.lua
+++ lua/u/config/commands.lua
@@ -1,4 +1,5 @@
 local user_command = require("u.nvim").user_command
 user_command("Json :set ft=json | %!python3 -m json.tool")
 user_command("Strip :call vlib#strip()")
+user_command("Bpath :echo expand('%:p')")
 
