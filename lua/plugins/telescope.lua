8f81a5cfc9f41b6e489616402bda5517d5c4354f
Marc Mazer <cramrezam@gmail.com>
2023-10-13 10:49:32 -0400

    Re-add key mappings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,6 +22,10 @@ return {
       local nmap = require("u.nvim").nmap
       nmap("<space>f", "<cmd>lua require('telescope.builtin').find_files()<cr>")
       nmap("<space>b", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+      nmap("<space>h", "<cmd>lua require('telescope.builtin').command_history()<cr>")
+      nmap("<space>H", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+      nmap("<space>m", "<cmd>lua require('telescope.builtin').man_pages()<cr>")
+      nmap("<space>q", "<cmd>lua require('telescope.builtin').quickfix()<cr>")
       nmap("<space>r", "<cmd>lua require('telescope.builtin').oldfiles()<cr>")
       nmap("<space>v", "<cmd>lua require('telescope').extensions.favourites.favourite_files()<cr>")
       nmap("<space>c", "<cmd>lua require('telescope').extensions.nvim_commands.commands()<cr>")
