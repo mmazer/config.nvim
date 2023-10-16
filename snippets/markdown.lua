6d9e14bafff59e078c53f9245b27d56c8d70c405
Marc Mazer <cramrezam@gmail.com>
2023-10-16 08:35:57 -0400

    Add markdown snippets for luasnip

---
+++ snippets/markdown.lua
@@ -0,0 +1,36 @@
+local date = require("u.date")
+return {
+  s("ffm",
+    fmt(
+      [[
+      title: {1}
+      id: {2}
+      created: {3}
+
+      # {4}
+
+      {5}
+      ]],
+      {
+        i(1, "title"),
+        f(date.timestamp_id),
+        f(date.timestamp),
+        rep(1),
+        i(0)
+      })
+  ),
+  s("jdt",
+    fmt(
+      [[
+      ### {1}
+
+      {2}
+      ]],
+      { f(date.date), i(0) }
+    )
+  ),
+  s("link",
+    fmt("[{1}]({2})", { i(1, "text"), i(2, "url") })
+  ),
+  s("todo", fmt("- [] {1}", { i(0) }))
+}
