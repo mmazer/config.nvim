9987c86eca8c61bbe44b683b9b3a505becdb0591
Marc Mazer <cramrezam@gmail.com>
2023-10-15 21:01:15 -0400

    Add simple timestamp ID function

--- lua/u/date.lua
+++ lua/u/date.lua
@@ -8,6 +8,10 @@ M.timestamp_utc = function()
   return os.date("!%Y-%m-%dT%H:%M:%S")
 end
 
+M.timestamp_id = function()
+  return os.date("%Y%m%d%H%M%S")
+end
+
 M.datetime = function()
   return os.date("%Y-%m-%d %H:%M")
 end
