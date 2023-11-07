12375e7c5efcce2ec6d50ef0053e10c1dfeedbfe
Marc Mazer <cramrezam@gmail.com>
2023-11-07 13:55:44 -0500

    Add user command to set yaml filetype

--- lua/u/config/commands.lua
+++ lua/u/config/commands.lua
@@ -2,4 +2,5 @@ local user_command = require("u.nvim").user_command
 user_command("Json :set ft=json | %!python3 -m json.tool")
 user_command("Strip :call vlib#strip()")
 user_command("Bpath :echo expand('%:p')")
+user_command("Yaml :set ft=yaml")
 
