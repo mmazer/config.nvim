6e9432c6ff9cecb88e95830e4dad94857a559bb4
Marc Mazer <cramrezam@gmail.com>
2023-11-09 14:32:28 -0500

    Add lua snippet for zinc plugin dependencies

--- snippets/zsh.lua
+++ snippets/zsh.lua
@@ -1,4 +1,17 @@
 return {
+  s({trig="pdeps", desc="Zinc plugin dependencies check"},
+    fmt(
+      [[
+      {1}_deps=({2})
+      if ! zn-assert-deps ${3}_deps; then
+          zn-plugin-status $(zn-plugin-name "$0") "missing one of ${4}_deps"
+          return
+      fi
+      {5}
+      ]],
+      { i(1, "plugin_name"), i(2, "dependencies"), rep(1), rep(1), i(0) }
+    )
+  ),
   s("zcmd?",
     fmt(
     [[
@@ -28,7 +41,7 @@ return {
     { i(0) }
     )
   ),
-   s("plugs",
+   s({trig="plugs",desc="Set zinc plugin status"},
     fmt(
     [[
     zn-plugin-status $(zn-plugin-name "$0") "{1}"
