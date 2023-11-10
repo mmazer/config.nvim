4a2450999bc5a6f29a1b754f1591e2aa8eccba2b
Marc Mazer <cramrezam@gmail.com>
2023-11-10 11:40:23 -0500

    Add Browse command to support :GBrowse
    The oil pluging disables netrw which is required by GBrowse

--- lua/u/config/commands.lua
+++ lua/u/config/commands.lua
@@ -3,4 +3,6 @@ user_command("Json :set ft=json | %!python3 -m json.tool")
 user_command("Strip :call vlib#strip()")
 user_command("Bpath :echo expand('%:p')")
 user_command("Yaml :set ft=yaml")
+-- Used in place of GBrowse
+vim.cmd([[ command! -nargs=1 Browse silent exec '!open "<args>"' ]])
 
