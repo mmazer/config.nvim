96d54402ec1cab0c7d75c1122eea9a0918302133
Marc Mazer <cramrezam@gmail.com>
2023-11-12 20:44:55 -0500

    Add key binding to open wiki style note links

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,5 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop=2
 vim.opt_local.wrap = true
 vim.opt_local.suffixesadd = ".txt,.md"
+vim.keymap.set({"n"}, "gz", function() vim.cmd(":normal vi]gf<CR>") end,
+  {silent = true, buffer=true})
