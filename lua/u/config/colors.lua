2b09ace770d8ee5c4f7d74099a0e9dd513ce059b
Marc Mazer <cramrezam@gmail.com>
2023-11-14 17:32:50 -0500

    Set colors based on terminal theme
    Settings must be kept in sync with alacritty and `alth`zsh function

--- lua/u/config/colors.lua
+++ lua/u/config/colors.lua
@@ -1,5 +1,16 @@
 vim.cmd("syntax enable")
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.g.rose_pine_variant = "base"
-vim.cmd("colorscheme rose-pine")
+vim.opt.termguicolors=true
+local term_theme=vim.env.TERM_THEME
+if (term_theme == nil or term_theme == '') then
+    term_theme = "dark"
+end
+local color_schemes={
+    light = "catppuccin-latte",
+    dark = "catppuccin-mocha"
+}
+local scheme = color_schemes[term_theme]
+if (scheme ~=  nil) then
+  vim.cmd("colorscheme "..scheme)
+else
+  print("no colorscheme found for theme: "..term_theme)
+end
