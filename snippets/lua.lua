28bd64b026b12d56813f74bc0df0d9ad575984ce
Marc Mazer <cramrezam@gmail.com>
2023-11-14 10:17:18 -0500

    Add lua snippets for vim.keymap.set

---
+++ snippets/lua.lua
@@ -0,0 +1,26 @@
+return {
+  s("km",fmt(
+    [[
+    vim.keymap.set({{"{1}"}}, "{2}", {3})
+    ]],
+    { i(1, "mod"), i(2, "keys"), i(3, "rhs") }
+  )),
+  s("kms",fmt(
+    [[
+    vim.keymap.set({{"{1}"}}, "{2}", {3}, {{silent=true}})
+    ]],
+    { i(1, "mod"), i(2, "keys"), i(3, "rhs") }
+  )),
+  s("nkm",fmt(
+    [[
+    vim.keymap.set({{"n"}}, "{1}", {2})
+    ]],
+    { i(1, "keys"), i(2, "rhs") }
+  )),
+  s("nkms",fmt(
+    [[
+    vim.keymap.set({{"n"}}, "{1}", {2}), {{silent=true}})
+    ]],
+    { i(1, "keys"), i(2, "rhs") }
+  ))
+}
