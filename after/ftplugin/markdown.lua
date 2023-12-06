d9a7e4d9489250514424db0b4716285eb9210ed4
Marc Mazer <cramrezam@gmail.com>
2023-12-05 20:33:57 -0500

    Add initial obsidian plugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,5 +2,3 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop=2
 vim.opt_local.wrap = true
 vim.opt_local.suffixesadd = ".txt,.md"
-vim.keymap.set({"n"}, "gz", function() vim.cmd(":normal vi]gf<CR>") end,
-  {silent = true, buffer=true})
