b447938d4c234354a1dfbbb7967cc5a259f34732
Marc Mazer <mmazer@freshbooks.com>
2024-10-03 10:13:02 -0400

    Add Dos2Unix command

--- lua/u/config/commands.lua
+++ lua/u/config/commands.lua
@@ -6,3 +6,4 @@ user_command("Yaml :set ft=yaml")
 -- Used in place of GBrowse
 vim.cmd([[ command! -nargs=1 Browse silent exec '!open "<args>"' ]])
 vim.cmd([[command! LocalTerm let s:term_dir=expand('%:p:h') | below new | call termopen([&shell], {'cwd': s:term_dir }) ]])
+vim.cmd([[command! Dos2Unix :%s/\r//g ]])
