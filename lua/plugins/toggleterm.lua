5982574a8dbb531b9d79d3a78af85abc11e44b10
Marc Mazer <mmazer@freshbooks.com>
2024-11-08 09:08:53 -0500

    Use tab layout for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,7 +12,7 @@ return {
       start_in_insert = true,
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       persist_size = true,
-      direction = "float",
+      direction = "tab",
       close_on_exit = true, -- close the terminal window when the process exits
       float_opts = {
         -- The border key is *almost* the same as "nvim_open_win"
