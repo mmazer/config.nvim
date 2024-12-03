436821ec5c698b90789ec6e85b7de56ec9eb6b18
Marc Mazer <mmazer@freshbooks.com>
2024-12-03 17:17:54 -0500

    Add custom resource specs to k8s.nvim

--- lua/plugins/k8s.lua
+++ lua/plugins/k8s.lua
@@ -1,7 +1,16 @@
 return {
   "mmazer/k8s.nvim",
   config = function()
-    require("kubectl").setup()
+    require("kubectl").setup({
+      resource_specs = {
+        sealedsecrets={},
+        datadogmetrics={shortnames={"ddmetrics"}},
+        helmcharts={},
+        helmreleases={shortnames={"hr"}},
+        helmrepositories={shortnames={"helmrepo"}},
+        kustomizations={shortnames={"ks"}}
+      }
+    })
     vim.keymap.set({"n"}, "<leader>k", ":Kubectl<CR>", {silent=true})
     vim.cmd("cab kube Kubectl")
   end
