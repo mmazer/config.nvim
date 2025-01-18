f4da214970a901d25df93638ad67fba9ef3fdef2
Marc Mazer <mmazer@freshbooks.com>
2025-01-18 11:14:47 -0500

    Add oil.nvim key map for opening current directory

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,6 +8,7 @@ return {
             ["<C-l>"] = false,
             ["<C-f>"] = "actions.refresh",
             ["<C-t>"] = false,
+            ["<leader>o"] = "<CMD>silent exec '!open ' . expand('%:p')[6:]<CR>",
           }
         })
         vim.keymap.set({"n"}, "-", "<CMD>Oil<CR>", {silent = true})
