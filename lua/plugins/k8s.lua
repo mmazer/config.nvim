2df0b367c15ab96ed816a07a2cdcac6219d71ed7
Marc Mazer <cramrezam@gmail.com>
2025-01-30 21:43:01 -0500

    Clean up plugins

--- lua/plugins/k8s.lua
+++ lua/plugins/k8s.lua
@@ -2,6 +2,12 @@ return {
   "mmazer/k8s.nvim",
   config = function()
     require("kubectl").setup({
+      user_commands = {
+        failedsched = {"get", "events","--field-selector", "reason=FailedScheduling"},
+        kustomizevents = {"get", "events","--field-selector", "involvedObject.kind=Kustomization"},
+        podowner = {"get", "pod", "--all-namespaces", "-o", "custom-columns=NAME:.metadata.name,CONTROLLER:.metadata.ownerReferences[].kind,NAMESPACE:.metadata.namespace"},
+        unhealthy = {"get", "events", "--field-selector", "reason=Unhealthy"}
+      },
       resource_specs = {
         backendconfigs={shortnames={"becfg"}},
         frontendconfigs={shortnames={"fecfg"}},
