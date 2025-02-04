ef95007c0f2c484aef174aac623f3e900c19ddc3
Marc Mazer <mmazer@freshbooks.com>
2025-02-04 18:07:43 -0500

    Add plenary.nvim as explicit plugin dependency

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,7 +1,8 @@
 return {
   dir = "~/.config/nvim/lua/u/plugins/obsidian",
   dependencies = {
-    "nvim-telescope/telescope.nvim"
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim"
   },
   config = function()
     require("obsidian").setup()
