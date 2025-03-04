c333e7cc8269ee081a7c56c4d208fe901f80e531
Marc Mazer <mmazer@freshbooks.com>
2025-03-04 11:03:39 -0500

    fix: add all namespaces scope to K8s user commands

--- lua/plugins/k8s.lua
+++ lua/plugins/k8s.lua
@@ -3,24 +3,26 @@ return {
   config = function()
     require("kubectl").setup({
       user_commands = {
-        failedsched = {"get", "events","--field-selector", "reason=FailedScheduling"},
-        kustomizevents = {"get", "events","--field-selector", "involvedObject.kind=Kustomization"},
-        podowner = {"get", "pod", "--all-namespaces", "-o", "custom-columns=NAME:.metadata.name,CONTROLLER:.metadata.ownerReferences[].kind,NAMESPACE:.metadata.namespace"},
-        unhealthy = {"get", "events", "--field-selector", "reason=Unhealthy"}
+        failedsched = { "get", "events", "--all-namespaces", "--field-selector", "reason=FailedScheduling" },
+        kustomizevents = { "get", "events", "--all-namespaces", "--field-selector", "involvedObject.kind=Kustomization" },
+        podowner = { "get", "pod", "--all-namespaces", "-o", "custom-columns=NAME:.metadata.name,CONTROLLER:.metadata.ownerReferences[].kind,NAMESPACE:.metadata.namespace" },
+        unhealthy = { "get", "events", "--field-selector", "reason=Unhealthy" }
       },
       resource_specs = {
-        backendconfigs={shortnames={"becfg"}},
-        frontendconfigs={shortnames={"fecfg"}},
-        sealedsecrets={},
-        datadogmetrics={shortnames={"ddm"}},
-        helmcharts={},
-        helmreleases={shortnames={"hr"}},
-        helmrepositories={shortnames={"helmrepo"}},
-        kustomizations={shortnames={"ks"}},
-        managedcertificates={shortnames={"mc"}},
+        backendconfigs = { shortnames = { "becfg" } },
+        frontendconfigs = { shortnames = { "fecfg" } },
+        sealedsecrets = {},
+        datadogmetrics = { shortnames = { "ddm" } },
+        helmcharts = {},
+        helmreleases = { shortnames = { "hr" } },
+        helmrepositories = { shortnames = { "helmrepo" } },
+        kustomizations = { shortnames = { "ks" } },
+        managedcertificates = { shortnames = { "mc" } },
+        iampolicy = { shortnames = { "iap" } },
+        iampolicymember = { shortnames = { "ipm" } }
       }
     })
-    vim.keymap.set({"n"}, "<leader>k", ":Kubectl<CR>", {silent=true})
+    vim.keymap.set({ "n" }, "<leader>k", ":Kubectl<CR>", { silent = true })
     vim.cmd("cab kube Kubectl")
   end
 }
