d083cf633997808e42ccac228ab2d03073af711e
Marc Mazer <mmazer@freshbooks.com>
2025-04-24 10:45:43 -0400

    feat: add checkmark snippet

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,4 +1,12 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local fmt = require("luasnip.extras.fmt").fmt
+
 return {
+  s("cmk", { t("✓") }),
   s("dt", f(function(...)
     return os.date("%Y-%m-%d")
   end)),
