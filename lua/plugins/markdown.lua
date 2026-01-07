4a42b3642ae420866afe0ad8d7538dbfb463b6b9
Marc Mazer <mmazer@freshbooks.com>
2026-01-07 16:23:39 -0500

    feat: add keymap to toggle render-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,4 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { "yom", function() require("render-markdown").toggle() end,  desc = "Toggle render markdown" },
+  }
 }
